8265d9cd4d59baac24f4f5d4aaa07485494c7578
Grzegorz Sparzak <g.sparzak@gmail.com>
2016-06-09 16:29:05 +0200

    mapka1

--- script/planner.js
+++ script/planner.js
@@ -7,4 +7,10 @@ var openPlanner = function (){
         display: 'none'
     });
 
+    var mapDiv = document.getElementById('mapka');
+    var map = new google.maps.Map(mapDiv, {
+        center: {lat: 51.540, lng: 15.546},
+        zoom: 5
+    });
+
 }
