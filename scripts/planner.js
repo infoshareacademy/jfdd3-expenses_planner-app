0535cd864563d48d4974dfc70325e42d6abc62b1
Grzegorz Sparzak <g.sparzak@gmail.com>
2016-06-09 21:40:00 +0200

    mapa z wyszukiwarką

--- scripts/planner.js
+++ scripts/planner.js
@@ -1,14 +1,59 @@
 'use strict';
 
-$('#planner').click(function() {
+$('#planner-icon').click(function() {
     $('.intro').css({
         display: 'none'
     });
+    $('#planner').css({
+        display: 'block'
+    });
 
-    var mapDiv = document.getElementById('mapka');
+    var mapDiv = document.getElementById('map');
     var map = new google.maps.Map(mapDiv, {
         center: {lat: 51.540, lng: 15.546},
         zoom: 5
+    });
+
+    var input = document.getElementById('pac-input');
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.bindTo('bounds', map);
+
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var infowindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+        map: map
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
+    });
+
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        var place = autocomplete.getPlace();
+        if (!place.geometry) {
+            return;
+        }
+
+        if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+        } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+        }
+
+        // Set the position of the marker using the place ID and location.
+        marker.setPlace({
+            placeId: place.place_id,
+            location: place.geometry.location
+        });
+        marker.setVisible(true);
+
+        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+            'Place ID: ' + place.place_id + '<br>' +
+            place.formatted_address);
+        infowindow.open(map, marker);
     });
 });
 
