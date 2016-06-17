9674cb0a2fa69e7d9243ac1fab3597b07fb3a022
Dawid <dawid.kubic@gmail.com>
2016-06-17 10:14:48 +0200

    creating active key + hover button

--- scripts/menu.js
+++ scripts/menu.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
 // Show chosen div, and hide all others
     $(".menu div").click(function () {
         $('.pages > div').hide();
-        $('.menu > div').css({"background-color": "white"});
+        $('.menu > div').removeClass('active');
         $('#' + $(this).attr('id') + "Container").show().css({"height": "90vh"}).addClass('animation');
-        $('#' + $(this).attr('id')).css({"background-color": "red"});
+        $('#' + $(this).attr('id')).addClass('active');
         $(".menu").css({"height": "10vh"});
         $(".floatflex").css({"align-self": "center", "padding": "0", "padding-left": "5vw"});
         $("#planner, #currency, #salary, #migration").css({
