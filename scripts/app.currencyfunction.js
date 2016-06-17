758a7c76742fdc0bdcfc3ec3fe5c8710429672fb
Natalia Waligora <fliberczibit@gmail.com>
2016-06-17 16:02:03 +0200

    wersja do merga

--- scripts/app.currencyfunction.js
+++ scripts/app.currencyfunction.js
@@ -1,9 +1,15 @@
 (function() {
     var app = angular.module('expensePlaner', []);
 
-    app.controller('travelPlaner', control);
- 
+    app.controller('currencyExchange', control);
+
 
     function control($scope){
+        debugger;
+        $scope.updateCurrencyName = function () {
+            debugger;
+            $scope.currencyName = $( "#selectSecondname option:selected" ).attr('label');
+
+        }
     }
 }());
