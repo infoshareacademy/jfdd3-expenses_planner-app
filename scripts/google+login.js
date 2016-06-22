0bc09ad0b979ce917120936d5fa69de0806fe7d4
Grzegorz Sparzak <g.sparzak@gmail.com>
2016-06-22 16:33:45 +0200

    on the way to log out

--- scripts/google+login.js
+++ scripts/google+login.js
@@ -4,7 +4,18 @@ function onSignIn(googleUser) {
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
+    var greeting = $('<div>').text("Witaj " + profile.getName());
+    var logIn = $('.g-signin2');
+    greeting.addClass('greeting greetingAnimation').appendTo(".pages").delay(2000).fadeOut(1000, function () {
+    $(this).remove();
+    });
+    logIn.fadeOut(1000, function () {
+        $(this).remove()});
 }
+
+var onSignOut = function() {
+    $('.g-signout').click(signOut());
+};
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
