aeb8a8148fd704b61f12b98e22cc3b8919b2bd3a
Grzegorz Sparzak <g.sparzak@gmail.com>
2016-06-23 16:32:11 +0200

    on the way to log out 2

--- scripts/google+login.js
+++ scripts/google+login.js
@@ -1,16 +1,19 @@
+
+var logIn = $('.g-signin2');
+var logOut = $('.g-signout');
+
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail());
-    var greeting = $('<div>').text("Witaj " + profile.getName());
-    var logIn = $('.g-signin2');
+    var greeting = $('<div>').append($('<span>')).addClass(".greetingSpan").text("Witaj " + profile.getName());
     greeting.addClass('greeting greetingAnimation').appendTo(".pages").delay(2000).fadeOut(1000, function () {
     $(this).remove();
     });
-    logIn.fadeOut(1000, function () {
-        $(this).remove()});
+    logIn.fadeOut(500);
+    logOut.fadeIn(500)
 }
 
 var onSignOut = function() {
@@ -21,5 +24,7 @@ function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
         console.log('User signed out.');
+        logIn.fadeIn(500);
+        logOut.fadeOut(500)
     });
 }
