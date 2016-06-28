61232a5cbf0a3431c730f7a7ef37c2aea63cb39f
Natalia Waligora <fliberczibit@gmail.com>
2016-06-28 09:56:07 +0200

    właczona czesc grzesia

--- app.js
+++ app.js
@@ -13,7 +13,11 @@
     }
 
     function ctrlSpreadSheet($scope, CurrenciesService) {
-        $scope.items = [];
+
+        $scope.items = localStorage.getItem('shoppingList')
+            ? JSON.parse(localStorage.getItem('shoppingList')) : [];
+
+        //$scope.items = [];
         var defaultCurrency;
 
         CurrenciesService.downloadCurrencies().then(function (data) {
@@ -28,25 +32,38 @@
             };
         }
 
-        $scope.licz = function(){
+        $scope.licz = function () {
             console.log($scope.items);
-            $scope.total = $scope.items.reduce(function(prevItem,currItem){
+            $scope.total = $scope.items.reduce(function (prevItem, currItem) {
                 return prevItem + currItem.price;
-            },0);
+            }, 0);
         };
 
-        $scope.removeItem = function(product){
-            return $scope.items.forEach(function(item,index){
-                if(item === product)
-                    $scope.items.splice(index,1)
-            })
+        $scope.updateStorage = function () {
 
+            localStorage.setItem('shoppingList', JSON.stringify($scope.items.map(function (item) {
+                return {
+                    name: item.name,
+                    price: item.price
+                }
+            })));
         };
 
-        $scope.addToList = function() {
+
+        $scope.removeItem = function (product) {
+            return $scope.items.forEach(function (item, index) {
+                if (item === product)
+                    $scope.items.splice(index, 1)
+            });
+            $scope.updateStorage();
+        };
+
+        $scope.addToList = function () {
             $scope.newProduct.isBought = true;
             var product = angular.copy($scope.newProduct);
             $scope.items.push(product);
+            $scope.updateStorage();
             resetProduct();
         };
-}})();
+    }
+})();
